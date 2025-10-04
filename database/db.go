7ce07f7a27d73783053d52479c82ed7902e40ac9
Nur Hanna <nurhanna01@users.noreply.github.com>
2025-10-04 09:59:44 +0700

    feat: implement configurable environment

--- database/db.go
+++ database/db.go
@@ -1,18 +1,31 @@
 package database
 
 import (
+	"fmt"
+	"os"
+
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 var (
   gormDB *gorm.DB
-  err    error
 )
 
 func InitDB() error {
-	dsn := "root:@tcp(127.0.0.1:3306)/mygo?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load()
+	if err !=nil {
+		fmt.Println("error load ENV file")
+	}
+
+	databaseHost  := os.Getenv("DB_HOST")
+	databaseUser := os.Getenv("DB_USER")
+	databasePort := os.Getenv("DB_PORT")
+	databasePassword := os.Getenv("DB_PASS")
+	databaseName  := os.Getenv("DB_NAME")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",	databaseUser,databasePassword,databaseHost, databasePort,databaseName)
   gormDB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
   if err != nil {
     return err
